d5525f510611d95b61335e5e76c71de58c25a5c6
Esemudia <57021444+Esemudia@users.noreply.github.com>
2024-05-27 16:49:43 +0100

    new index

--- english.php
+++ english.php
@@ -20,7 +20,7 @@ class English {
         $Myarray = $_SESSION['Myarray'] ?? [];
         $lga = [];
 
-        if ($level == 1) {
+       else if ($level == 1) {
             $query = "SELECT questions FROM question WHERE language='English'";
             $stmt = $this->dbh->query($query);
             $results = $stmt->fetchAll();
@@ -38,88 +38,74 @@ class English {
                 $response = "END No questions found for the selected language.";
             }
         } 
-
-        if ($level == 2) {
-            $query = "SELECT questions FROM question WHERE language='English'";
+        elseif ($level == 2) {
+            // Debugging statement to check textarray content
+            error_log("textarray[1]: " . print_r($textarray[1], true));
+            $query = "SELECT state FROM state";
             $stmt = $this->dbh->query($query);
-            $results = $stmt->fetchAll();
-
-            if (count($results) > 0) {
-                foreach ($results as $row) {
-                    $Myarray[] = $row['questions'];
+            $result3 = $stmt->fetchAll();
+            if (count($result3) > 0) {
+                foreach ($result3 as $row) {
+                    $state[] = $row['state'];
                 }
-                $_SESSION['Myarray'] = $Myarray;
-                $response = "CON {$Myarray[0]}\n";
-                $response .= "1. Yes\n";
-                $response .= "2. No\n";
-                
+                $_SESSION['state'] = $state;
+                if (isset($state[0], $state[1], $state[2])) {
+                    $response = "CON Select state your language:\n";
+                    $response .= "1. {$state[0]}\n";
+                    $response .= "2. {$state[1]}\n";
+                    $response .= "3. {$state[2]}\n";
+                } else {
+                    $response = "END Not enough states found.";
+                }
             } else {
-                $response = "END No questions found for the selected language.";
+                $response = "END No states found.";
             }
         } 
-        // elseif ($level == 2) {
-        //     $query = "SELECT * FROM state";
-        //     $stmt = $this->dbh->query($query);
-        //     $result3 = $stmt->fetchAll();
-        //     if (count($result3) > 0) {
-        //         foreach ($result3 as $row) {
-        //             $state[] = $row['state'];
-        //         }
-                
-        //         $response = "CON Select State:\n";
-        //             $response .= "1. {$state[0]}\n";
-        //             $response .= "2. {$state[1]}\n";
-        //             $response .= "3. {$state[2]}\n";
-        //         }
-        //     } else {
-        //         $response = "END No states found.";
-        //     }
-        // } 
-        // elseif ($level == 2) {
-        //     if ($textarray[2]== 1) { // Corrected index
-        //         $query = "SELECT lga FROM location WHERE state='Lagos'";
-        //         $stmt = $this->dbh->query($query);
-        //         $result3 = $stmt->fetchAll();
-        //         if (count($result3) > 0) {
-        //             $response = "CON Select location:\n";
-        //             $i = 1;
-        //             foreach ($result3 as $row) {
-        //                 $response .= $i++ . ". " . $row['lga'] . "\n";
-        //             }
-        //         } else {
-        //             $response = "END No LGAs found.";
-        //         }
-        //     }
-        //     if ($textarray[2]== 2){
-        //         $query = "SELECT lga FROM location WHERE state='Abuja'";
-        //         $stmt = $this->dbh->query($query);
-        //         $result3 = $stmt->fetchAll();
-        //         if (count($result3) > 0) {
-        //             $response = "CON Select location:\n";
-        //             $i = 1;
-        //             foreach ($result3 as $row) {
-        //                 $response .= $i++ . ". " . $row['lga'] . "\n";
-        //             }
-        //         } else {
-        //             $response = "END No LGAs found.";
-        //         }
-        //     }
-        //     if ($textarray[2]== 3){
-        //         $query = "SELECT lga FROM location WHERE state='Adamawa'";
-        //         $stmt = $this->dbh->query($query);
-        //         $result3 = $stmt->fetchAll();
-        //         if (count($result3) > 0) {
-        //             $response = "CON  Select location:\n";
-        //             $i = 1;
-        //             foreach ($result3 as $row) {
-        //                 $response .= $i++ . ". " . $row['lga'] . "\n";
-        //             }
-        //         } else {
-        //             $response = "END No LGAs found.";
-        //         }
-        //     }
-        // } 
         elseif ($level == 3) {
+            if ($textarray[2]== 1) { // Corrected index
+                $query = "SELECT lga FROM location WHERE state='Lagos'";
+                $stmt = $this->dbh->query($query);
+                $result3 = $stmt->fetchAll();
+                if (count($result3) > 0) {
+                    $response = "CON Select location:\n";
+                    $i = 1;
+                    foreach ($result3 as $row) {
+                        $response .= $i++ . ". " . $row['lga'] . "\n";
+                    }
+                } else {
+                    $response = "END No LGAs found.";
+                }
+            }
+            if ($textarray[2]== 2){
+                $query = "SELECT lga FROM location WHERE state='Abuja'";
+                $stmt = $this->dbh->query($query);
+                $result3 = $stmt->fetchAll();
+                if (count($result3) > 0) {
+                    $response = "CON Select location:\n";
+                    $i = 1;
+                    foreach ($result3 as $row) {
+                        $response .= $i++ . ". " . $row['lga'] . "\n";
+                    }
+                } else {
+                    $response = "END No LGAs found.";
+                }
+            }
+            if ($textarray[2]== 3){
+                $query = "SELECT lga FROM location WHERE state='Adamawa'";
+                $stmt = $this->dbh->query($query);
+                $result3 = $stmt->fetchAll();
+                if (count($result3) > 0) {
+                    $response = "CON  Select location:\n";
+                    $i = 1;
+                    foreach ($result3 as $row) {
+                        $response .= $i++ . ". " . $row['lga'] . "\n";
+                    }
+                } else {
+                    $response = "END No LGAs found.";
+                }
+            }
+        } 
+        elseif ($level == 4) {
             $response = "CON Select sex:\n";
             $response .= "1. Male\n";
             $response .= "2. Female\n";
