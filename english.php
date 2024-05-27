60d9ac4d6c6e317e7a511e82e67ace93b3117d37
Esemudia <57021444+Esemudia@users.noreply.github.com>
2024-05-27 05:05:47 +0100

    new index

---
+++ english.php
@@ -0,0 +1,42 @@
+<?php
+
+class english{
+
+    protected $text;
+    protected $sessionId;
+    function getmenu($textarray)  {
+        $level=count($textarray);
+    if ($level==1) {
+            $query = "SELECT questions FROM question WHERE language='English'";
+            $stmt = $dbh->query($query);
+            $results = $stmt->fetchAll();
+
+            if (count($results) > 0) {
+                foreach ($results as $row) {
+                    $Myarray[] = $row['questions'];
+                }
+                $response = "CON {$Myarray[0]}\n";
+                $response .= "1. Yes\n";
+                $response .= "2. No\n";            
+                
+            }
+    }
+
+    elseif ($level==2) {
+        $query = 'SELECT state FROM state';
+        $stmt = $dbh->query($query);
+        $result3 = $stmt->fetchAll();
+        if (count($result3) > 0) {
+            $id=1;
+            foreach ($result3 as $row) {
+                $reps[] = $id++ => $row['state'];
+            }
+            $response = "CON {$Myarray[1]}\n";
+            $response .= "1. {$reps[0]}\n";
+            $response .= "2. {$reps[1]}\n";
+            $response .= "3. {$reps[2]}\n";
+
+    }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $serviceCode = $_POST['serviceCode'] ?? '';
 $phoneNumber = $_POST['phoneNumber'] ?? '';
 $text = $_POST['text'] ?? '';
 
+$menu= new english();
 $response = '';
 $Myarray = [];
 $reps = [];
@@ -21,7 +22,6 @@ $input = [];
 
 try {
     $textarray=explode("*",$text);
-    $level=$textarray;
     if ($text=='') {
        
             $query = 'SELECT language FROM language';
@@ -42,37 +42,10 @@ try {
        else if ($text=='0') {
             $response = "END Your phone number is $phoneNumber";
         }
-       else if ($level==1) {
-            $query = "SELECT questions FROM question WHERE language='English'";
-            $stmt = $dbh->query($query);
-            $results = $stmt->fetchAll();
-
-            if (count($results) > 0) {
-                foreach ($results as $row) {
-                    $Myarray[] = $row['questions'];
-                }
-                $response = "CON {$Myarray[0]}\n";
-                $response .= "1. Yes\n";
-                $response .= "2. No\n";            
-                
-            }
-    }
-
-    elseif ($level==2) {
-        $query = 'SELECT state FROM state';
-        $stmt = $dbh->query($query);
-        $result3 = $stmt->fetchAll();
-        if (count($result3) > 0) {
-            $id=1;
-            foreach ($result3 as $row) {
-                $reps[] = $id++ => $row['state'];
-            }
-            $response = "CON {$Myarray[1]}\n";
-            $response .= "1. {$reps[0]}\n";
-            $response .= "2. {$reps[1]}\n";
-            $response .= "3. {$reps[2]}\n";
-
-    }
+        elseif ($textarray[0]) {
+            $menu->getmenu($textarray)
+        }
+     
    
 } catch (Exception $e) {
     error_log("Error executing query: " . $e->getMessage());
