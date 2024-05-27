71be7c9ea244a271d7ccf22ecb24c77bbf72d712
Esemudia <57021444+Esemudia@users.noreply.github.com>
2024-05-27 06:13:21 +0100

    new index

--- english.php
+++ english.php
@@ -1,14 +1,23 @@
 <?php
 
-class english{
+class english {
 
+    protected $dbh;
     protected $text;
     protected $sessionId;
-    function getmenu($textarray)  {
-        $level=count($textarray);
-    if ($level==1) {
+
+    public function __construct($dbh) {
+        $this->dbh = $dbh;
+    }
+
+    public function getmenu($textarray) {
+        $level = count($textarray);
+        $Myarray = [];
+        $response = '';
+
+        if ($level == 1) {
             $query = "SELECT questions FROM question WHERE language='English'";
-            $stmt = $dbh->query($query);
+            $stmt = $this->dbh->query($query);
             $results = $stmt->fetchAll();
 
             if (count($results) > 0) {
@@ -17,27 +26,25 @@ class english{
                 }
                 $response = "CON {$Myarray[0]}\n";
                 $response .= "1. Yes\n";
-                $response .= "2. No\n";            
-                
+                $response .= "2. No\n";
             }
-    }
-
-    elseif ($level==2) {
-        $query = 'SELECT state FROM state';
-        $stmt = $dbh->query($query);
-        $result3 = $stmt->fetchAll();
-        if (count($result3) > 0) {
-            $id=1;
-            foreach ($result3 as $row) {
-                $reps[] =  $row['state'];
+        } elseif ($level == 2) {
+            $query = 'SELECT state FROM state';
+            $stmt = $this->dbh->query($query);
+            $result3 = $stmt->fetchAll();
+            if (count($result3) > 0) {
+                $id = 1;
+                foreach ($result3 as $row) {
+                    $reps[] = $row['state'];
+                }
+                $response = "CON {$Myarray[1]}\n";
+                $response .= "1. {$reps[0]}\n";
+                $response .= "2. {$reps[1]}\n";
+                $response .= "3. {$reps[2]}\n";
             }
-            $response = "CON {$Myarray[1]}\n";
-            $response .= "1. {$reps[0]}\n";
-            $response .= "2. {$reps[1]}\n";
-            $response .= "3. {$reps[2]}\n";
+        }
 
+        return $response;
     }
-    }
-}
 }
 ?>
